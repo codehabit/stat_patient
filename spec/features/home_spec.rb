283526eb65dae6a626faa9fbcf57f6ce349ab9c9
David Oliver <david.oliver@codehabit.net>
2014-06-25 14:23:11 -0500

    Link to manage patients from home page

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -5,5 +5,16 @@ describe HomeController do
     visit root_path
     expect(page).to have_content "Welcome to Stat Patient"
   end
+
+  it "has a link to manage patients" do
+    visit root_path
+    expect(page).to have_link "Manage Patients"
+  end
+
+  it "links to the patients index" do
+    visit root_path
+    click_link "Manage Patients"
+    expect(current_path).to eq patients_path
+  end
 end
 
