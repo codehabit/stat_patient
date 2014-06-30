6dfe468635723c9c041e2f4a8581b85c6f6bfe09
David Oliver <david.oliver@codehabit.net>
2014-06-30 14:09:39 -0500

    Addind seeds for patients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+first_names = ["David", "John", "Sally", "Jane", "Martha", "Stuart", "Gregg", "Todd", "Suzanne", "Annika", "Bek", "Jonas", "Alex", "Rob", "Bill", "Ted", "Garth", "Wayne", "Mary", "Fred", "Josh", "Maggie", "Andrea", "Helen"]
+last_names = ["Oliver", "Johnson", "Williamson", "Wilson", "Johnston", "Broome", "Leach", "Chisman", "Suarez", "Jiminez", "Frakenstein", "Jones", "Smith", "Miller", "Lively", "Beckstrom", "Youngberg", "Kallenberg", "Davidson", "Ebeka", "Ronson", "Blake", "Townsend", "Pitt"]
+first_birthday = 30.years.ago
+
+first_names.each_with_index do |first, i|
+  Patient.create(first_name: first, last_name: last_names[i], date_of_birth: first_birthday + 1.month, email: "#{first.downcase}@statpatient.com")
+end
+
