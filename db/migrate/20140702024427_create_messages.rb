6ed13a79565666e802fc135d964152403042d410
Alex Leach <aleach84@gmail.com>
2014-07-01 22:01:26 -0500

    73491898 create messages table and model

---
+++ db/migrate/20140702024427_create_messages.rb
@@ -0,0 +1,12 @@
+class CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.string :case_number
+      t.references :patient
+      t.references :recipient, polymorphic: true
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
