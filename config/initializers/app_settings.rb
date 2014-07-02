104a798e65f02c1de5e353344535dda050c56f02
robb-broome <robb@teamvoice.com>
2014-07-02 16:42:59 -0500

    Settings for title so we can indicate the env

--- config/initializers/app_settings.rb
+++ config/initializers/app_settings.rb
@@ -1,1 +1,9 @@
 require 'app_settings'
+class AppSettings < Settingslogic
+  source "#{Rails.root}/config/application.yml"
+  namespace Rails.env
+
+  def env_title
+    self.title << " " << Rails.env
+  end
+end
