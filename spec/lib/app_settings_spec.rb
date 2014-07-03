e8f57cb225d27e636f427eeddc7e503d70fa26c6
David Oliver <david.oliver@codehabit.net>
2014-07-03 09:58:35 -0500

    Finished the change to app settings

--- spec/lib/app_settings_spec.rb
+++ spec/lib/app_settings_spec.rb
@@ -1,11 +1,6 @@
 require 'spec_helper'
 
 describe AppSettings do
-  it "shows the env" do
-    Rails.should_receive(:env).and_return "party"
-    expect(AppSettings.title).to eq "Stat Patient party"
-  end
-
   it "remembers the title" do
     expect(AppSettings.title).to eq "Stat Patient test"
   end
