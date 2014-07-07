6a3ba1c8b61a87c5c49b68b839dc13132982d109
David Oliver <david.oliver@codehabit.net>
2014-07-07 06:02:10 -0500

    AppSettings not behaving as I had expected

--- spec/lib/app_settings_spec.rb
+++ spec/lib/app_settings_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe AppSettings do
   it "remembers the title" do
-    expect(AppSettings.title).to eq "Stat Patient test"
+    expect(Rails).to receive(:env).and_return "test"
+    expect(AppSettings.env_title).to eq "Stat Patient test"
   end
 end
 
