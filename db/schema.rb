d23416b845cd9fd1bca02ec041903e2bc8e729ee
Alex Leach <aleach84@gmail.com>
2014-07-07 07:38:53 -0500

    74135240 add sender to messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707031705) do
+ActiveRecord::Schema.define(version: 20140707114903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,15 +29,16 @@ ActiveRecord::Schema.define(version: 20140707031705) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "subject"
+    t.integer  "sender_id"
   end
 
   create_table "patients", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "middle_initial"
+    t.date     "date_of_birth"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.datetime "date_of_birth"
     t.string   "title"
     t.string   "street"
     t.string   "street2"
