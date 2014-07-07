3942c211e48a5199cfa351e4ee0fb015814f466c
Alex Leach <aleach84@gmail.com>
2014-07-07 07:38:53 -0500

    74135240 add sender to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :patient
   belongs_to :recipient, polymorphic: true
+  belongs_to :sender, class_name: Practitioner
   has_attached_file :image, styles: {thumb: "200x200>"}
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   validates :recipient, presence: true
