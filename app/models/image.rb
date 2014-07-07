ed4ab38553976275bee722c2b475fcc378c51b68
David Oliver <david.oliver@codehabit.net>
2014-07-07 07:44:40 -0500

    73492656 Added the image to the show page.

---
+++ app/models/image.rb
@@ -0,0 +1,6 @@
+class Image < ActiveRecord::Base
+  has_attached_file :image, styles: {thumb: "200x200>"}
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  belongs_to :message
+end
+
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,8 @@ class Message < ActiveRecord::Base
   belongs_to :sender, class_name: Practitioner
   has_attached_file :image, styles: {thumb: "200x200>"}
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  # has_many :images
+  # accepts_nested_attributes_for :images
   validates :recipient, presence: true
 end
 
