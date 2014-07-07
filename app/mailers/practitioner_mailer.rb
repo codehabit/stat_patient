f88183b72b062273e506dd3bcb00f9e2fc5c26ef
Alex Leach <aleach84@gmail.com>
2014-07-07 11:43:11 -0500

    74135240 notify recipients of a message

---
+++ app/mailers/practitioner_mailer.rb
@@ -0,0 +1,9 @@
+class PractitionerMailer < ActionMailer::Base
+  default from: "noreply@statpatient.com"
+
+  def notification_email(message)
+    @practitioner = message.recipient
+    @message = message
+    mail(to: @practitioner.user.email, subject: "Message from StatPatient: #{@message.subject}")
+  end
+end
