d69280be3ec9a211b1487935393a56551bbe1ce9
David Oliver <david.oliver@codehabit.net>
2014-07-09 06:00:23 -0500

    Accidentally repeated a line

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,6 @@ Rails.application.configure do
     password:             'nJa7JydhmVqmpkRha8ZV',
     authentication:       'plain',
     enable_starttls_auto: true  }
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
