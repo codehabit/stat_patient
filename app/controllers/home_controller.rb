b7c421ce16ec63725b8c9d9704acb2701ad026af
robb-broome <robb@teamvoice.com>
2014-07-10 07:20:48 -0500

    Messages in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @received_cases= current_user.practitioner.received_cases
+    @practitioner = current_user.practitioner
+    @received_cases= @practitioner.received_cases
   end
 end
 
