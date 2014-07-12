bbe3772ad525f6b4b5e358fa009993c891f39923
robb-broome <robb@teamvoice.com>
2014-07-11 20:25:41 -0500

    Properly handle de-selecting a patient, reset session key
    Note: it's always necessary to reset the session key when data is
    reloaded, so that the session isn't set to a patient that is
    nonexistant.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,16 +10,20 @@ class ApplicationController < ActionController::Base
 
     @practitioner = current_user.practitioner
     @patients = @practitioner.patients
+    patient_param = params['patient']
 
-    if (patient_param = params['patient']).blank? || @patients.blank?
+    if patient_param == ""
       # patient intentionally removed, show all things
       session[:patient] = nil
       @patient_cases = @practitioner.involved_cases
 
-    elsif patient_id = patient_param || session[:patient]
+    elsif patient_id = (patient_param || session[:patient])
       @patient = Patient.find patient_id
       session[:patient] = @patient.id
       @patient_cases = @practitioner.involved_cases_for @patient
+    else
+      # nothing present (like at sign in)
+      @patient_cases = @practitioner.involved_cases
     end
 
     @collapsed = params[:collapsed] || session[:collapsed]
