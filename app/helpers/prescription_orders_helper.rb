3993770e68c67c1e8d265bc5bb5c0588362e8af6
robb-broome <robb@teamvoice.com>
2014-08-07 18:33:19 -0500

    76558070 formatting on prescriptions

--- app/helpers/prescription_orders_helper.rb
+++ app/helpers/prescription_orders_helper.rb
@@ -1,9 +1,3 @@
 module PrescriptionOrdersHelper
 
-  def le obj, attr, opts = {}
-    content_tag :div, class: 'row rx-print' do 
-      content_tag(:div, opts[:label] || attr.to_s.titleize, :class => 'col-sm-4 rx-attr-name') +
-      content_tag(:div, obj.send(attr), class: 'col-sm-8')
-    end
-  end
 end
