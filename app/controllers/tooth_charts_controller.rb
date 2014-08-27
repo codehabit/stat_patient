d29726d96c069bc1645c56d35506925998547ab3
Alex Leach <aleach84@gmail.com>
2014-08-27 13:39:13 -0500

    fix non-message bug for tooth charts in new cases

--- app/controllers/tooth_charts_controller.rb
+++ app/controllers/tooth_charts_controller.rb
@@ -7,15 +7,19 @@ class ToothChartsController < ApplicationController
   def update
     tooth_chart = ToothChart.find(tooth_chart_params[:id])
     markings_attributes = JSON.parse(tooth_chart_params["tooth_chart_markings_attributes"])
-    message = Message.new({body: make_custom_message(markings_attributes), sender: current_user.practitioner})
-    if tooth_chart.case
-      message.recipient = tooth_chart.case.originator == message.sender ? tooth_chart.case.recipient : tooth_chart.case.originator
-      message.patient = tooth_chart.case.patient
-      message.case = tooth_chart.case
+    response_json = {new_case: tooth_chart.case.nil?}
+    if tooth_chart_params["notes"]
+      message = Message.new({body: make_custom_message(markings_attributes), sender: current_user.practitioner})
+      if tooth_chart.case
+        message.recipient = tooth_chart.case.originator == message.sender ? tooth_chart.case.recipient : tooth_chart.case.originator
+        message.patient = tooth_chart.case.patient
+        message.case = tooth_chart.case
+      end
+      message.save
+      response_json[:message] = message.id
     end
-    message.save
     tooth_chart.update_attributes(tooth_chart_markings_attributes: markings_attributes)
-    render json: {message_id: message.id, new_case: tooth_chart.case.nil?}
+    render json: response_json
   end
 
   private
