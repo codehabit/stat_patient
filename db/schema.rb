996cecb5c9c5571f649a957af334a78877766b00
robb-broome <robb@teamvoice.com>
2014-08-27 13:42:13 -0500

    76371638 cleanup drug selection display

--- db/schema.rb
+++ db/schema.rb
@@ -81,17 +81,6 @@ ActiveRecord::Schema.define(version: 20140827133338) do
     t.datetime "last_activity_date"
   end
 
-  create_table "ceramics", force: true do |t|
-    t.string   "material"
-    t.string   "shade"
-    t.string   "occlusion"
-    t.string   "contact"
-    t.integer  "laboratory_id"
-    t.integer  "laboratory_oder_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "contacts", force: true do |t|
     t.string   "contactable_type"
     t.integer  "contactable_id"
@@ -136,7 +125,6 @@ ActiveRecord::Schema.define(version: 20140827133338) do
     t.integer  "laboratory_order_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "occlusion"
   end
 
   create_table "laboratory_orders", force: true do |t|
@@ -202,18 +190,6 @@ ActiveRecord::Schema.define(version: 20140827133338) do
     t.string   "gender"
     t.string   "marital_status"
     t.string   "social_security_number"
-  end
-
-  create_table "pfms", force: true do |t|
-    t.string   "material"
-    t.string   "margin"
-    t.string   "pontic"
-    t.string   "occlusion"
-    t.string   "contact"
-    t.integer  "laboratory_id"
-    t.integer  "laboratory_oder_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "practitioners", force: true do |t|
