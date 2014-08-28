89678021952372d305dd71a1302c644a69aa31cd
David Oliver <david.oliver@codehabit.net>
2014-08-28 07:00:03 -0500

    75216472 Typeahead for case subject
    TODO - make a little prettier

---
+++ lib/case_subject.rb
@@ -0,0 +1,3 @@
+class CaseSubject < ValueSet
+  value_set ["Endodontic evaluation", "Endodontic treatment", "Extraction", "Implant evaluation", "Implant placement", "Oral pathology Evaluation", "Oral pathology Biopsy", "Orthodontic Evaluation", "Orthodontic treatment", "Oral surgery", "Pedodontic evaluation", "Pedodontic treatment", "Periodontal evaluation", "Periodontal treatment", "Prosthodontic evaluation", "Prosthodontic treatment"]
+end
