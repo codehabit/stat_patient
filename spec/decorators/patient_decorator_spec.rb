5057dae7ddae0d2b68fba0b00d44dcbfd0c0ce4b
David Oliver <david.oliver@codehabit.net>
2014-09-03 07:18:46 -0500

    77900700 Basic success scenario test for labs

--- spec/decorators/patient_decorator_spec.rb
+++ spec/decorators/patient_decorator_spec.rb
@@ -16,5 +16,16 @@ describe PatientDecorator do
     patient = create(:patient, first_name: "Miles", last_name: "Davis", date_of_birth: birthday)
     expect(patient.decorate.full_name_with_dob).to eq "Miles Davis - #{birthday.strftime("%m/%d/%Y")}"
   end
+
+  it "doesn't have extra spaces" do
+    birthday = DateTime.new(1926, 5, 26)
+    patient = create(:patient, first_name: "Miles", last_name: "Davis", date_of_birth: birthday)
+    expect(patient.decorate.last_first_with_dob).to_not match(/\s{2,}/)
+  end
+
+  it "makes last, first with middle" do
+    patient = create(:patient, first_name: "Miles", last_name: "Davis", middle_initial: "X")
+    expect(patient.decorate.last_first_name).to eq "Davis, Miles X"
+  end
 end
 
