1ea110fc2cf9a48996af2bb43195ece09204f548
David Oliver <david.oliver@codehabit.net>
2015-09-14 07:21:03 -0500

    Users can now create new labs

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -7,11 +7,28 @@ class OrganizationsController < ApplicationController
     @organization = Organization.find params[:id]
   end
 
+  def create
+    organization = Organization.new
+    organization.attributes = organization_params
+    if organization.valid?
+      organization.save
+      redirect_to organizations_path
+    else
+      render action: :new
+    end
+  end
+
   def update
     organization = Organization.find params[:id]
     organization.update_attributes organization_params
     redirect_to organizations_path
+  end
 
+  def new
+    # TODO - make this detect the type from the path
+    # Right now, the only "ask" is for the ability to create a new laboratory.
+    @organization = Laboratory.new
+    @organization.addresses.build
   end
 
   def index
