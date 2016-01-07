04bfb6e47fe25d2ed2d11d0a97c2fba5c9144447
David Oliver <david.oliver@codehabit.net>
2016-01-07 06:38:48 -0600

    Strong params fix for signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
     self.resource = resource_class.new_with_session(hash || {}, session)
     if params[:user].present?
       practitioner_attrs = params[:user].delete(:practitioner)
+      practitioner_attrs = practitioner_attrs.permit!
       practitioner_attrs[:email] = params[:user][:email]
       self.resource.practitioner = Practitioner.create(practitioner_attrs)
     else
