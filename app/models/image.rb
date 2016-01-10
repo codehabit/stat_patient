721c8f8c56f67821249dd6c06ea8b25918b3f5cb
David Oliver <david.oliver@codehabit.net>
2016-01-10 17:14:44 -0600

    Use scaled version of the image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < Attachment
-  has_attached_file :asset, styles: {web_viewer: "800x800>", thumb: "200x200>"}
+  has_attached_file :asset, styles: {landscape: "1000x1000>", web_viewer: "800x800>", thumb: "200x200>"}
   validates_attachment :asset, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
 
